fd6d58a061798ad730051ae0812a52cb32bde1f1
hafijur-rahman1 <61210448+hafijur-rahman1@users.noreply.github.com>
2021-05-03 03:23:39 +0600

    app loaded

--- testproject/settings.py
+++ testproject/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'myapp'
+    # my app
+    
+    'myapp',
+    'album'
 ]
 
 MIDDLEWARE = [
